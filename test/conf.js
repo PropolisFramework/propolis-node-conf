4d061961b9f6466269c27a1ce6d901f742012af7
Jean-Nicolas Boulay <jnbdzjnbdz@gmail.com>
2017-10-25 12:42:45 -0400

    Added some tests for the setter.

--- test/conf.js
+++ test/conf.js
@@ -1,3 +1,9 @@
+/**
+ * @file Test script for the index.js.
+ * @version 1.0.0
+ * @author Jean-Nicolas Boulay <jn@yaloub.com>
+ */
+
 'use strict';
 
 let conf = require('../index');
@@ -14,7 +20,7 @@ let typeStr = function(obj) {
     return isArray(obj) ? 'array' : typeof obj;
 };
 
-describe('conf', function () {
+describe('conf', () => {
 
     let confDir = path.join(__dirname, 'conf/');
     let confDefaultDir = path.join(confDir, 'default/');
@@ -23,43 +29,71 @@ describe('conf', function () {
     let confStagingDir = path.join(confDir, 'staging/');
     let confProductionDir = path.join(confDir, 'production/');
 
-    it('List all the conf levels using directory names without specific conf/ path', function () {
+    it('Set configuration path using default path', () => {
+        conf.setConfPath();
+        expect(conf._rootConfPath)
+            .to
+            .equal(path.join(__dirname, 'conf'))
+    });
+
+    it('Set configuration path using set with param path', () => {
+        conf.setConfPath('../conf');
+        expect(conf._rootConfPath)
+            .to
+            .equal(path.join(__dirname, '../conf'))
+    });
+
+    it('Get configuration path using set by default', () => {
+        conf._rootConfPath = undefined;
+        expect(conf.getConfPath())
+            .to
+            .equal(path.join(__dirname, 'conf'))
+    });
+
+    it('Get configuration path using set by using param path', () => {
+        conf._rootConfPath = undefined;
+        expect(conf.getConfPath('../conf'))
+            .to
+            .equal(path.join(__dirname, '../conf'))
+    });
+
+    it('List all the conf levels using directory names without specific conf/ path', () => {
         expect(conf.getLevels())
             .to
             .equal(['common', 'default', 'dev', 'production', 'staging'])
     });
 
-    it('List all the conf levels using directory names with specific conf/ path', function () {
+    it('List all the conf levels using directory names with specific conf/ path', () => {
         expect(conf.getLevels(confDir))
             .to
             .equal(['common', 'default', 'dev', 'production', 'staging'])
     });
 
-    it('List all the config files inside conf level directories', function () {
+    it('List all the config files inside conf level directories', () => {
         expect(conf.getConfList())
             .to
             .equal(['main.json', 'other.json', 'default-extra.json'])
     });
 
-    it('List all the config files inside conf level directories', function () {
+    it('List all the config files inside conf level directories', () => {
         expect(conf.getConfList(confDevDir))
             .to
             .equal(['main.json', 'other.json', 'default-extra.json'])
     });
 
-    it('List all the config files inside conf level directories', function () {
+    it('List all the config files inside conf level directories', () => {
         expect(conf.getConf('main'))
             .to
             .equal({"level": "dev", "name": "main"})
     });
 
-    it('List all the config files inside conf level directories', function () {
+    it('List all the config files inside conf level directories', () => {
         expect(conf.getConf('main', 'other'))
             .to
             .equal({"main": {"level": "dev", "name": "main"}, "other": {"level": "dev", "name": "other"}})
     });
 
-    it('List all the config files inside conf level directories', function () {
+    it('List all the config files inside conf level directories', () => {
         expect(conf.getConf('main', 'other', 'default-extra'))
             .to
             .equal({
@@ -69,7 +103,7 @@ describe('conf', function () {
             })
     });
 
-    it('List all the config files inside conf level directories', function () {
+    it('List all the config files inside conf level directories', () => {
         expect(conf.getConf('main', 'other', 'default-extra', 'common-extra'))
             .to
             .equal({
