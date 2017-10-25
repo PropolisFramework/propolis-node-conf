54a7144dfcb81a2c1302f4db8ceb5a81ae745989
Jean-Nicolas Boulay <jnbdzjnbdz@gmail.com>
2017-10-25 12:42:04 -0400

    Added setter for conf path and added some more details in the comment sections.

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
+/**
+ * @file Module object for getting configuration files.
+ * @module conf
+ * @version 1.0.0
+ * @author Jean-Nicolas Boulay <jn@yaloub.com>
+ */
+
 'use strict';
 
 /**
  * Module dependencies.
  */
-let fs = require('fs');
-let path = require('path');
+let fs = require('fs'),
+    path = require('path');
 
 /**
- * Get the configuration path.
- * @param path
+ * Set module global variables.
+ */
+let rootConfPath,
+    confCache;
+
+/**
+ * Setter for configuration path.
+ * @since 1.0.0
+ * @param {string} arguments - Multi-params for generating path for conf/ directory.
+ * @return {string|*}
+ */
+let setConfPath = () => {
+    rootConfPath = (arguments.length === 0) ?
+                    path.join(__dirname, 'conf/') :
+                    path.join(__dirname, arguments);
+};
+
+/**
+ * Getter for configuration path.
+ * @since 1.0.0
+ * @param {string} arguments - Multi-params for generating path for conf/ directory.
  * @returns {string|*}
  */
-let getConfPath = (path) => {
-    let extraPath = (typeof path === 'undefined') ? '' : path;
-    return path.join(__dirname, 'test/conf/', extraPath);
+let getConfPath = () => {
+    if (typeof rootConfPath === 'undefined') {
+        setConfPath(arguments);
+    }
+    return rootConfPath;
 };
 
 /**
  * Get a list of all the configuration levels.
+ * @since 1.0.0
  * @return {array}
  */
 let getLevels = () => {
@@ -27,6 +56,7 @@ let getLevels = () => {
 
 /**
  * Get configuration list.
+ * @since 1.0.0
  * @param {string} confLevelPath - The configuration specific level path.
  * @return {string|array|*}
  */
@@ -34,9 +64,11 @@ let getConfList = (confLevelPath) => {
     let confPath = getConfPath(confLevelPath);
     return fs.readdirSync(confPath);
 };
+
 /**
- *
- * @param {string} arguments -
+ * Get configuration file content.
+ * @since 1.0.0
+ * @param {string} arguments - Multi-params for generating path for conf/ directory.
  * @return {object}
  */
 let getConf = () => {
@@ -55,14 +87,16 @@ let getConf = () => {
 getLevels();
 
 /**
- *
+ * @constant
  * @type {{getConfPath: (function(*=)), getLevels: (function()), getConfList: (function(string)), getConf: (function())}}
  */
-const methods = {
+/*const methods = {
+    "_rootConfPath": rootConfPath,
+    "_confCache": confCache,
     "getConfPath": getConfPath,
     "getLevels": getLevels,
     "getConfList": getConfList,
     "getConf": getConf
 };
 
-module.exports = methods;
+module.exports = methods;*/
